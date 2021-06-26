2a4808c669e6c475c76bc6a86c109acd187b3b8d
viettu2k <doanviettu2k@gmail.com>
2021-06-26 10:47:46 +0700

    add menu bar

--- src/MainRouter.jsx
+++ src/MainRouter.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home.component";
+import Menu from "./core/Menu.component";
 import Signup from "./user/Signup.component";
 import Signin from "./user/Signin.component";
 
 const MainRouter = () => (
     <div>
+        <Menu />
         <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/signup" component={Signup} />
