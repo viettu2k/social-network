9a45fad6c1f88b3721bc75d2c62121d442003333
viettu2k <doanviettu2k@gmail.com>
2021-06-30 15:51:17 +0700

    add validations for edit profile

--- src/user/EditPofile.component.jsx
+++ src/user/EditPofile.component.jsx
@@ -12,6 +12,7 @@ class EditPofile extends Component {
             email: "",
             password: "",
             redirectToProfile: false,
+            error: "",
         };
     }
     init = (userId) => {
@@ -35,6 +36,26 @@ class EditPofile extends Component {
         this.init(userId);
     }
 
+    isValid = () => {
+        const { name, email, password } = this.state;
+        if (name.length === 0) {
+            this.setState({ error: "Name is required" });
+            return false;
+        }
+        // email@domain.com
+        if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+            this.setState({
+                error: "A valid Email is required",
+            });
+            return false;
+        }
+        if (password.length >= 1 && password.length <= 5) {
+            this.setState({ error: "Password must be at least 6 characters" });
+            return false;
+        }
+        return true;
+    };
+
     handleChange = (name) => (event) => {
         this.setState({ [name]: event.target.value });
     };
@@ -42,23 +63,25 @@ class EditPofile extends Component {
     clickSubmit = (event) => {
         event.preventDefault();
 
-        const { name, email, password } = this.state;
-        const user = {
-            name,
-            email,
-            password: password || undefined,
-        };
-        // console.log(user);
-        const userId = this.props.match.params.userId;
-        const token = isAuthenticated().token;
+        if (this.isValid()) {
+            const { name, email, password } = this.state;
+            const user = {
+                name,
+                email,
+                password: password || undefined,
+            };
+            // console.log(user);
+            const userId = this.props.match.params.userId;
+            const token = isAuthenticated().token;
 
-        update(userId, token, user).then((data) => {
-            if (data.error) this.setState({ error: data.error });
-            else
-                this.setState({
-                    redirectToProfile: true,
-                });
-        });
+            update(userId, token, user).then((data) => {
+                if (data.error) this.setState({ error: data.error });
+                else
+                    this.setState({
+                        redirectToProfile: true,
+                    });
+            });
+        }
     };
 
     signupForm = (name, email, password) => (
@@ -100,7 +123,8 @@ class EditPofile extends Component {
     );
 
     render() {
-        const { id, name, email, password, redirectToProfile } = this.state;
+        const { id, name, email, password, redirectToProfile, error } =
+            this.state;
 
         if (redirectToProfile) {
             return <Redirect to={`/user/${id}`}></Redirect>;
@@ -108,6 +132,13 @@ class EditPofile extends Component {
 
         return (
             <div className="container">
+                <div
+                    className="alert alert-danger"
+                    style={{ display: error ? "" : "none" }}
+                >
+                    {error}
+                </div>
+
                 <h2 className="mt-5 mb-5">Edit Profile</h2>
                 {this.signupForm(name, email, password)}
             </div>
