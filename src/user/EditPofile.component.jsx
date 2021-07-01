163fc77e1130313d209a56ed16c1d4f9766f8e4a
viettu2k <doanviettu2k@gmail.com>
2021-07-01 10:15:49 +0700

    add edit user with photo

--- src/user/EditPofile.component.jsx
+++ src/user/EditPofile.component.jsx
@@ -13,6 +13,8 @@ class EditPofile extends Component {
             password: "",
             redirectToProfile: false,
             error: "",
+            fileSize: 0,
+            loading: false,
         };
     }
     init = (userId) => {
@@ -32,6 +34,7 @@ class EditPofile extends Component {
     };
 
     componentDidMount() {
+        this.userData = new FormData();
         const userId = this.props.match.params.userId;
         this.init(userId);
     }
@@ -57,24 +60,23 @@ class EditPofile extends Component {
     };
 
     handleChange = (name) => (event) => {
-        this.setState({ [name]: event.target.value });
+        this.setState({ error: "" });
+        const value =
+            name === "photo" ? event.target.files[0] : event.target.value;
+        const fileSize = name === "photo" ? event.target.files[0].size : 0;
+        this.userData.set(name, value);
+        this.setState({ [name]: value, fileSize });
     };
 
     clickSubmit = (event) => {
         event.preventDefault();
+        this.setState({ loading: true });
 
         if (this.isValid()) {
-            const { name, email, password } = this.state;
-            const user = {
-                name,
-                email,
-                password: password || undefined,
-            };
-            // console.log(user);
             const userId = this.props.match.params.userId;
             const token = isAuthenticated().token;
 
-            update(userId, token, user).then((data) => {
+            update(userId, token, this.userData).then((data) => {
                 if (data.error) this.setState({ error: data.error });
                 else
                     this.setState({
@@ -86,6 +88,15 @@ class EditPofile extends Component {
 
     signupForm = (name, email, password) => (
         <form>
+            <div className="form-group">
+                <label className="text-muted">Profile Photo</label>
+                <input
+                    onChange={this.handleChange("photo")}
+                    type="file"
+                    accept="image/*"
+                    className="form-control"
+                />
+            </div>
             <div className="form-group">
                 <label className="text-muted">Name</label>
                 <input
@@ -123,7 +134,7 @@ class EditPofile extends Component {
     );
 
     render() {
-        const { id, name, email, password, redirectToProfile, error } =
+        const { id, name, email, password, redirectToProfile, error, loading } =
             this.state;
 
         if (redirectToProfile) {
@@ -140,6 +151,15 @@ class EditPofile extends Component {
                 </div>
 
                 <h2 className="mt-5 mb-5">Edit Profile</h2>
+
+                {loading ? (
+                    <div className="jumbotron text-center">
+                        <h2>Loading...</h2>
+                    </div>
+                ) : (
+                    ""
+                )}
+
                 {this.signupForm(name, email, password)}
             </div>
         );
