e7a005637aa97aeef97995d342fb0e328447a621
viettu2k <doanviettu2k@gmail.com>
2021-07-08 10:49:30 +0700

    default post image

--- src/core/Home.component.jsx
+++ src/core/Home.component.jsx
@@ -6,7 +6,7 @@ const Home = () => {
         <div>
             <div className="jumbotron">
                 <h2>Home</h2>
-                <p className="lead">Welcome to React Frontend</p>
+                <p className="lead">Welcome to Meme Social Network</p>
             </div>
             <div className="container">
                 <Posts />
--- src/post/Posts.component.jsx
+++ src/post/Posts.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { list } from "./apiPost";
-// import DefaultProfile from "../images/avatar.jpg";
+import DefaultPost from "../images/meme.jpg";
 
 class Posts extends Component {
     constructor() {
@@ -21,31 +21,54 @@ class Posts extends Component {
         });
     }
 
-    renderPosts = (posts) => (
-        <div className="row">
-            {posts.map((post, i) => (
-                <div className="card col-md-4" key={i}>
-                    {/* <img
-                        style={{ height: "200px", width: "auto" }}
-                        className="img-thumbnail"
-                        src={`${process.env.REACT_APP_API_URL}/user/photo/${user._id}`}
-                        onError={(i) => (i.target.src = `${DefaultProfile}`)}
-                        alt={user.name}
-                    /> */}
-                    <div className="card-body">
-                        <h5 className="card-title">{post.title}</h5>
-                        <p className="card-text">{post.body}</p>
-                        <Link
-                            to={`/posts/${post._id}`}
-                            className="btn btn-raised btn-primary btn-sm"
-                        >
-                            Read more
-                        </Link>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
+    renderPosts = (posts) => {
+        return (
+            <div className="row">
+                {posts.map((post, i) => {
+                    const posterId = post.postedBy
+                        ? `/user/${post.postedBy._id}`
+                        : "";
+                    const posterName = post.postedBy
+                        ? post.postedBy.name
+                        : "Unknown";
+
+                    return (
+                        <div className="card col-md-4" key={i}>
+                            <div className="card-body">
+                                <img
+                                    src={`${process.env.REACT_APP_API_URL}/post/photo/${post._id}`}
+                                    alt={post.title}
+                                    onError={(i) =>
+                                        (i.target.src = `${DefaultPost}`)
+                                    }
+                                    className="img-thumbnail mb-3"
+                                    style={{ height: "200px", width: "auto" }}
+                                ></img>
+                                <h5 className="card-title">{post.title}</h5>
+                                <p className="card-text">
+                                    {post.body.substring(0, 100)}
+                                </p>
+                                <br />
+                                <p className="font-italic mark">
+                                    Posted by{" "}
+                                    <Link to={`${posterId}`}>
+                                        {posterName}{" "}
+                                    </Link>
+                                    on {new Date(post.created).toDateString()}
+                                </p>
+                                <Link
+                                    to={`/posts/${post._id}`}
+                                    className="btn btn-raised btn-primary btn-sm"
+                                >
+                                    Read more
+                                </Link>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    };
 
     render() {
         const { posts } = this.state;
