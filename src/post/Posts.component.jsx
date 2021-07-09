28686cf4247049b719ef17f1a37204f988d90c67
viettu2k <doanviettu2k@gmail.com>
2021-07-09 10:35:07 +0700

    add delete post

--- src/post/Posts.component.jsx
+++ src/post/Posts.component.jsx
@@ -33,32 +33,36 @@ class Posts extends Component {
                         : "Unknown";
 
                     return (
-                        <div className="card-body">
-                            <img
-                                src={`${process.env.REACT_APP_API_URL}/post/photo/${post._id}`}
-                                alt={post.title}
-                                onError={(i) =>
-                                    (i.target.src = `${DefaultPost}`)
-                                }
-                                className="img-thumbnail mb-3"
-                                style={{ height: "200px", width: "auto" }}
-                            ></img>
-                            <h5 className="card-title">{post.title}</h5>
-                            <p className="card-text">
-                                {post.body.substring(0, 100)}
-                            </p>
-                            <br />
-                            <p className="font-italic mark">
-                                Posted by{" "}
-                                <Link to={`${posterId}`}>{posterName} </Link>
-                                on {new Date(post.created).toDateString()}
-                            </p>
-                            <Link
-                                to={`/post/${post._id}`}
-                                className="btn btn-raised btn-primary btn-sm"
-                            >
-                                Read more
-                            </Link>
+                        <div className="card col-md-4" key={i}>
+                            <div className="card-body">
+                                <img
+                                    src={`${process.env.REACT_APP_API_URL}/post/photo/${post._id}`}
+                                    alt={post.title}
+                                    onError={(i) =>
+                                        (i.target.src = `${DefaultPost}`)
+                                    }
+                                    className="img-thunbnail mb-3"
+                                    style={{ height: "200px", width: "100%" }}
+                                />
+                                <h5 className="card-title">{post.title}</h5>
+                                <p className="card-text">
+                                    {post.body.substring(0, 100)}
+                                </p>
+                                <br />
+                                <p className="font-italic mark">
+                                    Posted by{" "}
+                                    <Link to={`${posterId}`}>
+                                        {posterName}{" "}
+                                    </Link>
+                                    on {new Date(post.created).toDateString()}
+                                </p>
+                                <Link
+                                    to={`/post/${post._id}`}
+                                    className="btn btn-raised btn-primary btn-sm"
+                                >
+                                    Read more
+                                </Link>
+                            </div>
                         </div>
                     );
                 })}
--- src/post/SinglePost.component.jsx
+++ src/post/SinglePost.component.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import { singlePost } from "./apiPost";
-import { Link } from "react-router-dom";
+import { singlePost, remove } from "./apiPost";
+import { Link, Redirect } from "react-router-dom";
 import DefaultPost from "../images/meme.jpg";
+import { isAuthenticated } from "../auth";
 
 class SinglePost extends Component {
     state = {
         post: "",
+        redirectToHome: false,
     };
 
     componentDidMount = () => {
@@ -19,42 +21,81 @@ class SinglePost extends Component {
         });
     };
 
+    deletePost = () => {
+        const postId = this.props.match.params.postId;
+        const token = isAuthenticated().token;
+        remove(postId, token).then((data) => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                this.setState({ redirectToHome: true });
+            }
+        });
+    };
+
+    deleteConfirmed = () => {
+        let answer = window.confirm(
+            "Are you sure you want to delete your post?"
+        );
+        if (answer) {
+            this.deletePost();
+        }
+    };
+
     renderPost = (post) => {
         const posterId = post.postedBy ? `/user/${post.postedBy._id}` : "";
         const posterName = post.postedBy ? post.postedBy.name : "Unknown";
         return (
-            <div className="card col-md-4">
-                <div className="card-body">
-                    <img
-                        src={`${process.env.REACT_APP_API_URL}/post/photo/${post._id}`}
-                        alt={post.title}
-                        onError={(i) => (i.target.src = `${DefaultPost}`)}
-                        className="img-thumbnail mb-3"
-                        style={{
-                            height: "300px",
-                            width: "100%",
-                            objectFit: "cover",
-                        }}
-                    ></img>
-                    <p className="card-text">{post.body}</p>
-                    <br />
-                    <p className="font-italic mark">
-                        Posted by <Link to={`${posterId}`}>{posterName} </Link>
-                        on {new Date(post.created).toDateString()}
-                    </p>
+            <div className="card-body">
+                <img
+                    src={`${process.env.REACT_APP_API_URL}/post/photo/${post._id}`}
+                    alt={post.title}
+                    onError={(i) => (i.target.src = `${DefaultPost}`)}
+                    className="img-thumbnail mb-3"
+                    style={{
+                        height: "300px",
+                        width: "100%",
+                        objectFit: "cover",
+                    }}
+                />
+                <p className="card-text">{post.body}</p>
+                <br />
+                <p className="font-italic mark">
+                    Posted by <Link to={`${posterId}`}>{posterName} </Link>
+                    on {new Date(post.created).toDateString()}
+                </p>
+                <div className="d-inline-block">
                     <Link
                         to={`/`}
-                        className="btn btn-raised btn-primary btn-sm"
+                        className="btn btn-raised btn-primary btn-sm mr-5"
                     >
                         Back to posts
                     </Link>
+                    {isAuthenticated().user &&
+                        isAuthenticated().user._id === post.postedBy._id && (
+                            <>
+                                <button className="btn btn-raised btn-success mr-5">
+                                    Update Post
+                                </button>
+                                <button
+                                    onClick={this.deleteConfirmed}
+                                    className="btn btn-raised btn-danger"
+                                >
+                                    Delete Post
+                                </button>
+                            </>
+                        )}
                 </div>
             </div>
         );
     };
 
     render() {
-        const { post } = this.state;
+        const { post, redirectToHome } = this.state;
+
+        if (redirectToHome) {
+            return <Redirect to={`/`}></Redirect>;
+        }
         return (
             <div className="container">
                 <h2 className="display-2 mt-5 mb-5">{post.title}</h2>
